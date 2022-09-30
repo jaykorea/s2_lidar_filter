7581e4d0cdae30fba350321c24fd6596a2302beb
jay <jhjo1997@naver.com>
2022-09-30 20:16:59 +0900

    added log

--- node_filter.cpp
+++ node_filter.cpp
@@ -30,7 +30,7 @@ if (!input_scan.ranges.empty()) {
     for (unsigned int i = int(floor(las_mid_ran - deg_60)); i < int(floor(las_mid_ran+deg_60)); i++) { // left view of lidar frame <---|(0 degree)
         filtered_scan.ranges[i] = NAN;
 }
-        filtered_scan.header.frame_id = "base_laser_link";
+        filtered_scan.header.frame_id = "base_scan";
         filtered_scan.header.stamp = start_time;
 
     //ROS_INFO("ranges size :  %d", input_scan.ranges.size());
@@ -57,7 +57,7 @@ void input_scan_sub_callback(const sensor_msgs::LaserScan& input_scan)
 int main(int argc, char** argv){
 ros::init(argc, argv, "rplidar_filter_node");
 ros::NodeHandle n;
-input_scan_sub = n.subscribe("/scan", 50, input_scan_sub_callback);
+input_scan_sub = n.subscribe("/scan_rp", 50, input_scan_sub_callback);
 filtered_scan_pub = n.advertise<sensor_msgs::LaserScan>("/scan_rp_filtered", 50);
 ros::Rate loop_rate(20);
 
